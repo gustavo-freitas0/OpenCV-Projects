1e9c795d684beaa6c873b456bdb92cd26a2e578b
Gustavo Freitas <gprofi98@gmail.com>
2024-09-16 11:23:35 -0300

    Add others projects

---
+++ ImageProcessing_exploration.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+
+
+class ImageProcessing:
+
+    def __init__(self):
+        print('class Image Processing')
+
+    def filter(self, image: np.ndarray, filter_: str) -> np.ndarray:
+        """
+
+        :param image:
+        :param filter_:
+        :return:
+        """
+        match filter_:
+            case 'bilateral':
+                # highly effective in noise removal while keeping edges sharp. But the operation is slower compared
+                # to other filters.
+                cv.bilateralFilter(image, 9, 75, 75)
+
+        return image
+
+    def live_camera(self) -> None:
+        camera = cv.VideoCapture(0)
+
+        if camera.isOpened():
+            while cv.waitKey(1) == -1:
+                ret, frame = camera.read()
+                if ret:
+                    # apply here your image processing
+
+                    cv.imshow(f'Camera {camera.getBackendName()} image', frame)
+
+
+if __name__ == "__main__":
+    print('Code has been started')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OpenCV-Projects
 Repository of Image Processing and Computer Vision projects using OpenCV
+
+
